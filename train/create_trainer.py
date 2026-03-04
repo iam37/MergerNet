b002f876f671648de93303249ab579b02ac71fc6
iam37 <isaac.moskowitz@yale.edu>
2026-03-04 15:46:33 -0600

    typ

--- train/create_trainer.py
+++ train/create_trainer.py
@@ -23,7 +23,7 @@ def create_trainer(model, optimizer, criterion, loaders, device, use_scheduler=T
 
     if use_scheduler:
         #torch_lr_scheduler = CosineAnnealingLR(optimizer, T_max=40)
-        cheduler = ReduceLROnPlateau(optimizer, mode="min", patience=2, threshold=1e-4, min_lr=1e-7)
+        scheduler = ReduceLROnPlateau(optimizer, mode="min", patience=2, threshold=1e-4, min_lr=1e-7)
         #scheduler = CosineAnnealingLR(optimizer, T_max = 15)
         #scheduler = LRScheduler(torch_lr_scheduler)
         #warmup_scheduler = LinearLR(optimizer, start_factor=0.1, total_iters=5)
